62e2d2903306f8fb9876095112031578dac53cb8
angelina-yp <angelinyshechka@mail.ru>
2025-02-14 15:44:32 +0300

    hw-4

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,3 +6,21 @@
   преобразовываясь в number
 
 */
+let minAge1 = 18;
+let maxAge1 = 60;
+let age1 = "60";
+
+let age2 = Number(age1);
+
+if (isNaN(age2)) { 
+  console.log("Incorrect data type");}
+  else if(age2 < minAge1) {
+  console.log("You don't have access cause your age is " + age2 + " It's less then ");
+  } else if (age2 >=  minAge1 && age2 < maxAge1) {
+    console.log("Welcome  !");
+  } else if (age2 > maxAge1) {
+    console.log("Keep calm and look Culture channel");
+  } else {
+    console.log("Technical work");
+  }
+ 
