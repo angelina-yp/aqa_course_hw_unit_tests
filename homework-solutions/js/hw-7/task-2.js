c36d63fdc3a9e41b6f8ad3f209bd332347c2c5ed
angelina-yp <angelinyshechka@mail.ru>
2025-02-28 20:27:41 +0300

    HW-7

--- homework-solutions/js/hw-7/task-2.js
+++ homework-solutions/js/hw-7/task-2.js
@@ -5,8 +5,20 @@
 
 function isPalindrom(word) {
   // Ваш код
-}
+  if (typeof word !== 'string') {
+    return false; 
+  }
 
+  let word_1 = word.split('').reverse().join('');
+  if(word_1.toLowerCase() === word.toLowerCase() && word!= " ") {
+  console.log(true);
+    return true;
+  } 
+  console.log(false);
+  return false;
+}
+//let word1 = ' '
+//isPalindrom(word1)
 /*
  2. findLongestWords()
  Написать функцию, которая принимает предложение (слова разделенные только пробелами) в качестве параметра 
@@ -16,6 +28,28 @@ function isPalindrom(word) {
 
 function findLongestWords(sentence) {
   // Ваш код
-}
+  if (typeof sentence !== 'string') {
+    return []; 
+  }
+
+  let array = sentence.split(' ');
+  let maxLength = 0;
+  let maxword = [];
+  for (let element of array) {
+    if (element.length > 0) { 
+      maxLength = Math.max(maxLength, element.length);
+    }
+  }
+  //console.log(maxLength);
+  for (let element of array) {
+    if (element.length === maxLength && element.length > 0) {
+      maxword.push(element);
+    } 
+  }
+   // console.log(maxword);
+  return maxword
+}  
+let sentence1 = ' '
+findLongestWords(sentence1)
 
 export { isPalindrom, findLongestWords };
